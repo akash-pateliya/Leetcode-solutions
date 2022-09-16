dc9a731024e6a878518af969429ebe308e69f730
Akash Pateliya <akashpateliya00@gmail.com>
2022-09-16 18:59:59 +0530

    Time: 9 ms (24.02%), Space: 47.8 MB (37.76%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -3,12 +3,29 @@ class Solution {
         int n = matrix.length;
         
         int[][] dp = new int[n][n];
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
+        for(int i=0; i<n; i++){
+            dp[n-1][i] = matrix[n-1][i];
         }
+        
+        for(int row = n-2; row >= 0; row--){
+            for(int col = n-1; col >= 0; col--){
+                int down = matrix[row][col] + dp[row+1][col];
+                int leftDiagonal = Integer.MAX_VALUE;
+                int rightDiagonal = Integer.MAX_VALUE;
+                if(col - 1 >= 0){
+                    leftDiagonal = matrix[row][col] + dp[row+1][col-1];
+                }
+                if(col + 1 < n){
+                    rightDiagonal = matrix[row][col] + dp[row+1][col+1];
+                }
+                
+                dp[row][col] = Math.min(down, Math.min(leftDiagonal, rightDiagonal));
+            }
+        }
+        
         int min = Integer.MAX_VALUE;
         for(int i=0; i<n; i++){
-            min = Math.min(min, getMinFallingPathSum(0, i, matrix, dp));
+            min = Math.min(dp[0][i], min);
         }
         return min;
     }
