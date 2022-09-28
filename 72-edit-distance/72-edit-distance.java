0ea87de0ae274c9b67b65630a81383e9333a7492
Akash Pateliya <akash.pateliya@outlook.com>
2022-09-28 22:51:21 +0530

    Time: 8 ms (70.67%), Space: 44.7 MB (72.35%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -9,32 +9,32 @@ class Solution {
             Arrays.fill(arr, -1);
         }
         
-        return getMin(m-1, n-1, word1, word2, dp);
+        return getMin(m, n, word1, word2, dp);
     }
     
     int getMin(int i, int j, String word1, String word2, int[][] dp){
         
-        if(i<0){
-            return j+1;
+        if(i == 0){
+            return j;
         }
-        if(j<0){
-            return i+1;
+        if(j == 0){
+            return i;
         }
         
         if(dp[i][j] != -1){
             return dp[i][j];
         }
         
-        if(word1.charAt(i) == word2.charAt(j)){
+        if(word1.charAt(i-1) == word2.charAt(j-1)){
             dp[i][j] = getMin(i-1, j-1, word1, word2, dp);
             return dp[i][j];
         }
         else{
-            int insert = 1 + getMin(i, j-1, word1, word2, dp);
-            int replace = 1 + getMin(i-1, j-1, word1, word2, dp);
-            int remove = 1 + getMin(i-1, j, word1, word2, dp);
+            int insert = getMin(i, j-1, word1, word2, dp);
+            int replace = getMin(i-1, j-1, word1, word2, dp);
+            int remove = getMin(i-1, j, word1, word2, dp);
             
-            dp[i][j] = Math.min(insert, Math.min(replace, remove));
+            dp[i][j] = 1 + Math.min(insert, Math.min(replace, remove));
             return dp[i][j];
         }
     }
