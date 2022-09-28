12a72481598e4b6fdef47bdbec4ec81c35876e85
Akash Pateliya <akash.pateliya@outlook.com>
2022-09-28 18:07:11 +0530

    Time: 43 ms (18.96%), Space: 49.8 MB (73.30%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -4,10 +4,27 @@ class Solution {
         int n = t.length();
         
         int[][] dp = new int[m+1][n+1];
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
+        
+        for(int i=0; i<=m; i++){
+            dp[i][0] = 1;
         }
-        return getDistinctSubsequence(s.length(), t.length(), s, t, dp);
+        
+        for(int i=1; i<=n; i++){
+            dp[0][i] = 0;
+        }
+        
+        for(int i=1; i<=m; i++){
+            for(int j =1; j<=n; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                        dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                }
+                else{
+                        dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[m][n];//getDistinctSubsequence(s.length(), t.length(), s, t, dp);
     }
     
     int getDistinctSubsequence(int i, int j, String s, String t, int[][] dp){
