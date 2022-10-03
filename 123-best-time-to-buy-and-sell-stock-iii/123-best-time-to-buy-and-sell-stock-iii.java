505938b56288ffc275a4ee696e6c2cc6e3e8802c
Akash Pateliya <akash.pateliya@outlook.com>
2022-10-03 16:11:33 +0530

    Time: 273 ms (15.80%), Space: 124.3 MB (26.28%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,14 +1,41 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int n = prices.length;
+        
+
         int[][][] dp = new int[prices.length+1][2][3];
         
-        for(int[][] twoD: dp){
-            for(int[] oneD: twoD){
-                Arrays.fill(oneD, -1);
+        for(int i=0; i<n; i++){
+            for(int j = 0; j<=1; j++){
+                dp[i][j][0] = 0;
             }
         }
         
-        return getMaxProfit(0, 1, 2, prices, dp);
+        for(int i=0; i<=1; i++){
+            for(int j=0; j<=2; j++){
+                dp[n][i][j] = 0;
+            }
+        }
+        
+        for(int i=n-1; i >= 0; i--){
+            for(int buy=0; buy<=1; buy++){
+                for(int totalTrasaction=1; totalTrasaction <= 2; totalTrasaction++){
+                    if(buy == 1){
+                        int includeBuy = -prices[i] + dp[i+1][0][totalTrasaction];
+                        int excludeBuy = dp[i+1][1][totalTrasaction];
+
+                        dp[i][buy][totalTrasaction] = Math.max(includeBuy, excludeBuy);
+                    }
+                    else{
+                        int includeSell = prices[i] + dp[i+1][1][totalTrasaction-1];
+                        int excludeSell = dp[i+1][0][totalTrasaction];
+
+                        dp[i][buy][totalTrasaction] = Math.max(includeSell, excludeSell);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
     
     int getMaxProfit(int i, int buy,int totalTrasaction, int[] prices, int[][][] dp){
