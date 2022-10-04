a58c733c6f4b2e361bf57dfeb78749d20da3ffbc
Akash Pateliya <akash.pateliya@outlook.com>
2022-10-04 16:23:28 +0530

    Time: 8 ms (78.70%), Space: 44.4 MB (47.79%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,20 +1,36 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp = new int[nums.length+1][nums.length+1];
-  
         
-        for(int i=nums.length-1; i >= 0; i--){
-            for(int prev=i-1; prev >= -1; prev--){
-                int len = dp[i+1][prev+1];
-                if(prev == -1 || nums[i] > nums[prev]){
-                    len = Math.max(len,1 + dp[i+1][i+1]);
-                }
-
-                dp[i][prev+1] = len;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        
+        for(int i=1; i<nums.length; i++){
+            if(list.get(list.size() - 1) < nums[i]){
+                list.add(nums[i]);
+            }
+            else{
+                int idx = getLowerBound(nums[i], list);
+                list.set(idx, nums[i]);
             }
         }
         
-        return dp[0][0];
+        return list.size();
+        
+//         int[][] dp = new int[nums.length+1][nums.length+1];
+  
+        
+//         for(int i=nums.length-1; i >= 0; i--){
+//             for(int prev=i-1; prev >= -1; prev--){
+//                 int len = dp[i+1][prev+1];
+//                 if(prev == -1 || nums[i] > nums[prev]){
+//                     len = Math.max(len,1 + dp[i+1][i+1]);
+//                 }
+
+//                 dp[i][prev+1] = len;
+//             }
+//         }
+        
+//         return dp[0][0];
     }
     
     int getMaxLen(int i,int prev,int[] nums, int[][] dp){
@@ -34,5 +50,27 @@ class Solution {
         dp[i][prev+1] = len;
         
         return dp[i][prev+1];
+    }
+    
+    int getLowerBound(int target, ArrayList<Integer> nums){
+        int start = 0;
+        int end = nums.size() - 1;
+        
+        while(start <= end){
+            int mid =  start + (end - start) / 2;
+            
+            if(nums.get(mid) == target){
+                return mid;
+            }
+            else if(nums.get(mid) < target){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        
+        return start;
+        
     }
 }
