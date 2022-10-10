222a6e4e62fab36a34b60f642d65b3a321de5561
Akash Pateliya <akash.pateliya@outlook.com>
2022-10-10 17:50:26 +0530

    Time: 340 ms (18.98%), Space: 96 MB (26.41%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,66 +1,43 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        int[] previousSmaller = getPreviousSmaller(heights);
         int[] nextSmaller = getNextSmaller(heights);
-        int[] previousSmaller = getpreviousSmaller(heights);
-        
         int maxArea = Integer.MIN_VALUE;
-        
         for(int i=0; i<heights.length; i++){
             if(nextSmaller[i] == -1){
                 nextSmaller[i] = heights.length;
             }
-            int area = (nextSmaller[i] - previousSmaller[i] - 1) * heights[i];
+            int area = heights[i] * (nextSmaller[i] - previousSmaller[i] - 1);
             maxArea = Math.max(area, maxArea);
         }
-        
         return maxArea;
     }
     
-    int[] getNextSmaller(int[] heights){
-        int[] ans = new int[heights.length];
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i=heights.length - 1; i>=0; i--){
-            
-            while(!stack.empty() && heights[stack.peek()] >= heights[i]){
+    int[] getPreviousSmaller(int[] arr){
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] ans = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]){
                 stack.pop();
             }
             
-            if(stack.empty()){
-                ans[i] = -1;
-            }
-            else{
-                ans[i] = stack.peek();
-            }
-            
+            ans[i] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(i);
         }
-        
         return ans;
     }
     
-    int[] getpreviousSmaller(int[] heights){
-        int[] ans = new int[heights.length];
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i=0; i<heights.length; i++){
-            
-            while(!stack.empty() && heights[stack.peek()] >= heights[i]){
+    int[] getNextSmaller(int[] arr){
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] ans = new int[arr.length];
+        for(int i=arr.length - 1; i >= 0; i--){
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]){
                 stack.pop();
             }
             
-            if(stack.empty()){
-                ans[i] = -1;
-            }
-            else{
-                ans[i] = stack.peek();
-            }
-            
+            ans[i] = stack.isEmpty() ? -1 : stack.peek();
             stack.push(i);
         }
-        
         return ans;
     }
 }
