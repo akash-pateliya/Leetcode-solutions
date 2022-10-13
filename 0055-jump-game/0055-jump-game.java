057843b0faf545b6672265e72eda63f2ddd67fe8
Akash Pateliya <akash.pateliya@outlook.com>
2022-10-13 13:04:07 +0530

    Time: 109 ms (25.43%), Space: 67.8 MB (53.91%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,9 +1,32 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
+        int n = nums.length;
+        boolean[] dp = new boolean[n];
         
-        return isPossible(0, nums, dp);
+        dp[n-1] = true;
+        
+        for(int i=n-2; i>=0; i--){
+            if(nums[i] == 0){
+                dp[i] = false;
+                continue;
+            }
+            int maxJump = i + nums[i];
+            boolean flag = false;
+            for(int jump = i+1; jump <= maxJump; jump++){
+                if(jump < nums.length && dp[jump]){
+                    flag = true;
+                    dp[i] = true;
+                    break;
+                }
+            }
+            
+            if(flag){
+               continue; 
+            }
+            dp[i] = false;
+        }
+        
+        return dp[0];
     }
     
     boolean isPossible(int i, int[] nums, int[] dp){
